86652edb9d643de3912505446a736df8b82da8ed
nathaliab03 <nathaliabitencourt03@gmail.com>
2023-11-19 13:06:58 -0300

    Desenvolvimento do código PokemonApiDb.js para efetuar a busca e exibir os resultados

--- backend/src/models/pokemonApiDb.js
+++ backend/src/models/pokemonApiDb.js
@@ -1,10 +1,27 @@
 const axios = require('axios')
 
-const pokemonData = () => {
-    axios.get(`https://pokeapi.co/api/v2/pokemon/ditto`)
+const pokemonName = 'Charizard'.toLowerCase() // Pokemon API é case sensitive, dados precisam ser em minúsculo
+
+ const pokemonData = () => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     .then(function(response){
-        console.log(response.data)
-    })
+        const setPokemon = {
+            nome: pokemonName,
+            id: response.data.id,
+            type: response.data.types[0].type.name,
+            img: response.data.sprites.front_default
+        }
+
+        showPokemonData(setPokemon)
+            
+    }) 
+}
+
+function showPokemonData(setPokemon) {
+    console.log(setPokemon.nome)
+    console.log(setPokemon.id)
+    console.log(setPokemon.type)
+    console.log(setPokemon.img)
 }
 
 pokemonData()
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const pokemonData = require('./models/pokemonApiDb')
 
 const router = express.Router();
 
-router.get('/', (require, response) => response.status(200).send('router ok'))
+router.get('/', (require, response) => response.status(200).send(pokemonData))
 
 module.exports = router
